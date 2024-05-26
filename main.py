8898f5730f55f55230fbaf511887a7887d47b276
conorbowles51 <conorbowles2000@gmail.com>
2024-05-26 19:17:23 +0100

    camera

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from settings import *
 import moderngl as mgl
 import pygame as pg
 import sys
+
+from settings import *
+from shader_program import ShaderProgram
+from scene import Scene
+from player import Player
 
 class VoxelEngine:
     def __init__(self):
@@ -21,19 +25,43 @@ class VoxelEngine:
         self.delta_time = 0
         self.time = 0
 
+        pg.event.set_grab(True)
+        pg.mouse.set_visible(False)
+
         self.is_running = True
+        self.on_init()
+
+    def on_init(self):
+        self.player = Player(self)
+        self.shader_program = ShaderProgram(self)
+        self.scene = Scene(self)
 
     def update(self):
-        pass
+        self.player.update()
+        self.shader_program.update()
+        self.scene.update()
+
+        self.delta_time = self.clock.tick()
+        self.time = pg.time.get_ticks() * 0.001
+        pg.display.set_caption(f'{self.clock.get_fps() :.0f}')
 
     def render(self):
-        pass
+        self.ctx.clear(color=BG_COLOR)
+        self.scene.render()
+        pg.display.flip()
 
     def handle_events(self):
-        pass
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                self.is_running = False
 
     def run(self):
-        pass
+        while self.is_running:
+            self.handle_events()
+            self.update()
+            self.render()
+        pg.quit()
+        sys.exit()
 
 if __name__ == '__main__':
     app = VoxelEngine()
--- settings.py
+++ settings.py
@@ -3,4 +3,29 @@ import numpy as np
 import glm
 import math
 
-WIN_RES = glm.vec2(1920, 1080)
+# window resolution
+WIN_RES = glm.vec2(1080, 720)
+
+# chunk
+CHUNK_SIZE = 32
+H_CHUNK_SIZE = CHUNK_SIZE // 2
+CHUNK_AREA = CHUNK_SIZE * CHUNK_SIZE
+CHUNK_VOL = CHUNK_AREA * CHUNK_SIZE
+
+# camera
+ASPECT_RATIO = WIN_RES.x / WIN_RES.y
+FOV_DEG = 50
+V_FOV = glm.radians(FOV_DEG)
+H_FOV = 2 * math.atan(math.tan(V_FOV * 0.5) * ASPECT_RATIO)
+NEAR = 0.1
+FAR = 2000.0
+PITCH_MAX = glm.radians(89)
+
+# player
+PLAYER_SPEED = 0.005
+PLAYER_ROT_SPEED = 0.003
+PLAYER_POS = glm.vec3(H_CHUNK_SIZE, CHUNK_SIZE, 1.5 * CHUNK_SIZE)
+MOUSE_SENSITIVITY = 0.002
+
+# colors
+BG_COLOR = glm.vec3(0.1, 0.16, 0.25)
